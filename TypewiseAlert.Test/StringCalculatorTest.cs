3b925789f7ea92e08f282cc420c95fdb953aaba4
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-09 15:57:20 +0530

    Update StringCalculatorTest.cs

--- TypewiseAlert.Test/StringCalculatorTest.cs
+++ TypewiseAlert.Test/StringCalculatorTest.cs
@@ -2,3 +2,17 @@ void GivenEmptyString0isExpected()
 {
   int expectedValue = Assert.True(TypewiseAlert.add("") == TypewiseAlert.0)
 }
+
+
+//code implementation
+
+class Calculate
+{
+    static int add(string data)
+    {
+      if(data == "")
+        return 0;
+      
+      throw new NotImplementedException();
+    }
+}
