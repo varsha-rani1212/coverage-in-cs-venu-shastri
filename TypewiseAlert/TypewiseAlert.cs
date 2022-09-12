b8fcb10ec2932445ab5b1e5df4c38cc5842129a1
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-12 13:11:04 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -74,11 +74,16 @@ namespace TypewiseAlert
     
     public static void sendToEmail(BreachType breachType) {
       string recepient = "a.b@c.com";  
-      switch(breachType) {
-        case BreachType.TOO_LOW:
-          break;
-        case BreachType.TOO_HIGH:
-          break;
+      if(breachType == BreachType.TOO_LOW){
+        Console.WriteLine("To: {}\n", recepient);
+        Console.WriteLine("Hi, the temperature is too low\n");
+      }
+      
+      if(breachType == BreachType.TOO_HIGH){
+        Console.WriteLine("To: {}\n", recepient);
+        Console.WriteLine("Hi, the temperature is too high \n");
+      }
+      
       }
     }
   }
