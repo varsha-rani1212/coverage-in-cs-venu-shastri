0282e8d2eb178d065d9a30e96c6426768391437d
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-12 10:59:18 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -17,9 +17,12 @@ namespace TypewiseAlert.Test
       Assert.True(TypewiseAlert.inferBreach(15, 10, 20) ==
            TypewiseAlert.BreachType.NORMAL);
       
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 15) ==
+           TypewiseAlert.BreachType.NORMAL);
+      
      // Assert.True(TypewiseAlert.sendToEmail(BreachType.TOO_LOW));
       
-      TypewiseAlert.sendToController(TOO_LOW);
+      //TypewiseAlert.sendToController(TOO_LOW);
       
       
       //checkAndAlert(TO_CONTROLLER,{PASSIVE_COOLING,"tata"}, 123);
