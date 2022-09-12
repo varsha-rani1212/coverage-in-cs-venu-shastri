0b84c92358f8266bc1f65dadb7bcef12fe6e11c5
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-12 13:03:27 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -75,12 +75,8 @@ namespace TypewiseAlert
       string recepient = "a.b@c.com";
       switch(breachType) {
         case BreachType.TOO_LOW:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too low\n");
           break;
         case BreachType.TOO_HIGH:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too high \n");
           break;
         case BreachType.NORMAL:
           break;
